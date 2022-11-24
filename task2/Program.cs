2ac478d0f974453d6e6a1ed8bdd5c5f7d0a7879d
Ildar <ildar.azmatov@ya.ru>
2022-11-24 14:52:29 +0500

    проверка на квадрат числа

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int sum = numberB * numberB;
+if (numberA == sum)
+{
+Console.WriteLine("Yes");
+    }
+else
+{
+    Console.WriteLine("NO");
+}
