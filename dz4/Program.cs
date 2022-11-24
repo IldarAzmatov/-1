bfcb386ae5362748905f45ac0879a289a6b4cb5d
Ildar <ildar.azmatov@ya.ru>
2022-11-24 16:42:14 +0500

    задача 4, максимальное из трех чисел

---
+++ dz4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Input numberA");
+Console.WriteLine("Input numberB");
+Console.WriteLine("Input numberC");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB && numberA>numberC)
+{
+    Console.WriteLine(numberA);
+    if(numberB>numberA && numberB>numberC)
+    {
+        Console.WriteLine(numberB);
+    }
+}
+else{
+    Console.WriteLine(numberC);
+}
