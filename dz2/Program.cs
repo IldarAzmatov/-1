474849d91bb4e2cae78ba284c8935d54cb31932d
Ildar <ildar.azmatov@ya.ru>
2022-11-24 16:22:19 +0500

    задача 2, определение большего числа

---
+++ dz2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input numberA");
+Console.WriteLine("Input numberB");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB)
+{
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.Write(numberB);
+}
