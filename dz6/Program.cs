3ffe8934aabdb897c431638de126608401896736
Ildar <ildar.azmatov@ya.ru>
2022-11-24 17:03:46 +0500

    задача 6, определение четности

---
+++ dz6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+ Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
