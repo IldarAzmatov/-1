bf2960dffcc11383309f40ea8bdc2bdb1f923d5c
Ildar <ildar.azmatov@ya.ru>
2022-11-24 16:01:29 +0500

    Трехзначное число

---
+++ task5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное чило");
+int N = Convert.ToInt32(Console.ReadLine());
+if ( N < 1000 && N > 99 )
+{
+     Console.WriteLine("Последняя цифра "  +N%10);
+}
+else 
+{
+    Console.WriteLine("Нужно было ввести трехзначное число");
+}
